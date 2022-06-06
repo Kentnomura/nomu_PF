1e8283f16c5dae77cbf7dcd32d64ec1f8326fffa
Kentnomura <kentojaga0706@icloud.com>
2022-06-06 02:19:37 +0000

    ログイン機能　レイアウト以外

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,23 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+#ログインしたら
+
   def after_sign_in_path_for(resource)
     about_path
   end
 
+#ログアウトしたら
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   protected
 
+#メールアドレスを鍵としてもたせる
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
