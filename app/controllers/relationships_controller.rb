958e44efe87dab2b45985d039de392568586de05
Kentnomura <kentojaga0706@icloud.com>
2022-06-14 01:28:01 +0000

    comment 非同期化　途中

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,19 +4,19 @@ class RelationshipsController < ApplicationController
     current_user.follow(params[:user_id])
     redirect_to request.referer
   end
-  
+
   # フォロー外す
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
+    redirect_to request.referer
   end
-  
+
   # フォロー　一覧
   def followings
     user = User.find(params[:user_id])
     @users = user.followings
   end
-  
+
   # フォロワー　一覧
   def followers
     user = User.find(params[:user_id])
